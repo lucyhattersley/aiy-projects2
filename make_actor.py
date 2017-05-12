55c16f7fbbc3c0af1d0b8f870d2c1124c2c32201
LUCY HATTERSLEY <lucy.a.hattersley@gmail.com>
2017-05-12 14:09:39 +0100

    added lights on/lights off

--- make_actor.py
+++ make_actor.py
@@ -3,4 +3,8 @@
     # =========================================
 
     actor.add_keyword('light on', GpioWrite(26, True))
+    actor.add_keyword('lights on', GpioWrite(26, True))
+
     actor.add_keyword('light off', GpioWrite(26, False))
+    actor.add_keyword('lights off', GpioWrite(26, False))
+    
