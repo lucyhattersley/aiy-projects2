e12624594066a2d849a957cb9953ea34c15a5bbc
LUCY HATTERSLEY <lucy.a.hattersley@gmail.com>
2017-05-12 13:37:29 +0100

    changed gpio pin to 26

--- make_actor.py
+++ make_actor.py
@@ -2,5 +2,5 @@
     # Makers! Add your own voice commands here.
     # =========================================
 
-    actor.add_keyword('light on', GpioWrite(4, True))
-    actor.add_keyword('light off', GpioWrite(4, False))
+    actor.add_keyword('light on', GpioWrite(26, True))
+    actor.add_keyword('light off', GpioWrite(26, False))
